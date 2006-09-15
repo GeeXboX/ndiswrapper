951c1fb6cbbb60becb5d78d4613b499b6d8ed906
Mathieu Schroeter <mathieu.schroeter@mycable.ch>
2006-09-15 13:30:56 +0000

    cosmetic: remove the declarations of function

--- ndiswrapper.c
+++ ndiswrapper.c
@@ -92,67 +92,6 @@ static inline int my_mkdir(const char *path) {
 #endif
 }
 
-/* inf installation */
-int install(const char *inf);
-int isInstalled(const char *name);
-int loadinf(const char *filename);
-int initStrings(void);
-int processPCIFuzz(void);
-void addPCIFuzzEntry(const char *vendor, const char *device,
-                     const char *subvendor, const char *subdevice,
-                     const char *bt);
-int addReg(const char *reg_name, char param_tab[][STRBUFFER], unsigned int *k);
-
-/* driver tools */
-int remove(const char *name);
-
-/* parsers */
-int parseVersion(void);
-int parseMfr(void);
-int parseVendor(const char *flavour, const char *vendor_name);
-int parseID(const char *id, int *bt, char *vendor,
-            char *device, char *subvendor, char *subdevice);
-int parseDevice(const char *flavour, const char *device_sect,
-                const char *device, const char *vendor,
-                const char *subvendor, const char *subdevice);
-
-/* hashing processing */
-void getKeyVal(const char *line, char tmp[2][STRBUFFER]);
-char *getString(char *s);
-char *getVersion(char *s);
-char *getFuzzlist(char *s);
-char *getBuslist(char *s);
-char *getFixlist(char *s);
-void def_strings(const char *key, const char *val);
-void def_version(const char *key, const char *val);
-void def_fuzzlist(const char *key, const char *val);
-void def_buslist(const char *key, const char *val);
-
-/* files processing */
-int copyfiles(const char *copy_name);
-void copy_file(char *file);
-int copy(const char *file_src, const char *file_dst, int mod);
-int finddir(char *file);
-int findfile(const char *dir, char *file);
-int file_exists(const char *file);
-int rmtree(const char *dir);
-
-/* strings processing */
-char *uc(char *data);
-char *lc(char *data);
-char *trim(char *s);
-char *stripquotes(char *s);
-char *remComment(char *s);
-char *substStr(char *s);
-
-/* others */
-int regex(const char *str_request, const char *str_regex,
-          char rmatch[][STRBUFFER], ...);
-struct DEF_SECTION *getSection(const char *needle);
-void unisort(char tab[][STRBUFFER], unsigned int *last);
-void usage(void);
-
-
 /* global variables */
 unsigned int nb_sections = 0;
 char *confdir = CONFDIR;
